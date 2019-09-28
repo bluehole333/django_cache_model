180ac4495a7adf08395e1f188dcaabeaf2b77b10
bluehole333 <bluehole333@foxmail.com>
2019-09-28 14:44:33 +0800

    generate_cache_key

--- engine.py
+++ engine.py
@@ -25,3 +25,27 @@ for engine_name, engine in ENGINS.iteritems():
     engine_class = engine['class']
     engine_conf = engine['config']
     app[engine_name] = import_by_name(engine_class)(engine_conf)
+
+
+class Engine(object):
+
+    def __init__(self):
+        super(Engine, self).__init__()
+
+    @classmethod
+    def generate_cache_key(cls, pkey):
+        """
+        生成key
+        sign_key:
+            如果需要刷新此model的所有cache，在model定义中修改 sign_key 即可
+        """
+        if hasattr(cls, "sign_key"):
+            return KEY_PREFIX + cls.sign_key + "|" + cls.__module__ + "." + cls.__name__ + '|' + str(pkey)
+        else:
+            return KEY_PREFIX + "|" + cls.__module__ + "." + cls.__name__ + '|' + str(pkey)
+
+    def get_pkey(self):
+        """
+        根据子类对象获取key
+        """
+        return self.__class__.generate_cache_key(self.id)
