6280786cef508aafe1d8e04e20c4752713886dab
bluehole333 <bluehole333@foxmail.com>
2019-09-29 13:28:36 +0800

    get or put

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@
 """
 数据存储封装 可以添加多个缓存来写入
 """
+import pickle
 from conf import ENGINS
 
 KEY_PREFIX = "BLUEHOLE_CACHE_MODEL_PACKAGE_"
@@ -31,6 +32,7 @@ class Engine(object):
 
     def __init__(self):
         super(Engine, self).__init__()
+        self.save_list = []
 
     @classmethod
     def generate_cache_key(cls, pkey):
@@ -49,3 +51,42 @@ class Engine(object):
         根据子类对象获取key
         """
         return self.__class__.generate_cache_key(self.id)
+
+    @classmethod
+    def get(cls, pkey):
+        """
+        cache_key:cache key
+        pkey:uid 唯一标示
+        """
+        data = None
+        level = 0
+        cache_key = cls.generate_cache_key(pkey)
+        # 从存储层获取dumps后的对象数据  对应这配置文件的 现在就配置了 memcached  还可以有其他的 mysql redis等等
+        for engine_name in cls.save_list:
+            # 根据配置的引擎 获取一个对象 比如memcache的实例
+            engine_obj = app[engine_name]
+            level += 1
+            data = engine_obj.get_data(cls, cache_key)
+            # 如果取到了 就不再 遍历
+            if data is not None:
+                break
+        # 如果实在取不到就是没有数据
+        if data is None:
+            return None
+
+        # 获取到dumps数据，转换成为对象实例  这时候对象是 字典类型的
+        # 如果从底层数据库中读取出来的  再缓存到更上层cache中
+        obj = pickle.loads(data)
+        if level > 1:
+            top_engine_obj = app[ENGINS.keys()[0]]
+            top_engine_obj.put_data(cls, cache_key, data, False)
+
+        return obj
+
+    def put(self):
+        cls = self.__class__
+        data = pickle.dumps(self)
+        pkey = self.get_pkey()
+        for engine_name in cls.save_list:
+            engine_obj = app[engine_name]
+            engine_obj.put_data(cls, pkey, data, False)
