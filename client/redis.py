01aa75a65eb420c37fc4887668ffe4b6d2b4b2f0
bluehole333 <bluehole333@foxmail.com>
2019-10-12 12:06:54 +0800

    redis 客户端

--- client/redis.py
+++ client/redis.py
@@ -22,3 +22,53 @@ class PickledRedis(StrictRedis):
         # 先dumps 转成二进制 序列化 1和2是二进制  HIGHEST_PROTOCOL:是2  快而剩空间
 
         return super(PickledRedis, self).set(name, pickle.dumps(value, pickle.HIGHEST_PROTOCOL), ex, px, nx, xx)
+
+
+class RedisClient(object):
+
+    def __init__(self, config):
+        """
+        servers is a string like "IP:PORT; IP:PORT"
+        """
+        self.master = None
+        self.slaves = []
+        self.timeout = 0
+        self.db = 1
+        self.try_times = config.get("TRY_TIMES", 1)
+
+        self._set_master_slavers(config['masters'])
+        self._set_master_slavers(config['slaves'])
+
+    def _set_master_slavers(self, servers):
+        """
+        set master and slavers
+        """
+        if isinstance(servers, str):
+            try:
+                host, port = servers.split(":")
+                self.master = PickledRedis(host=host, port=int(port), db=self.db)
+            except (ValueError, TypeError):
+                pass
+            return
+
+        for server in servers:
+            try:
+                host, port = server.split(":")
+                rclient = PickledRedis(host=host, port=int(port), db=self.db)
+            except (ValueError, TypeError):
+                pass
+            else:
+                self.slaves.append(rclient)
+
+    def get_data(self, model_cls, pkey, default=None):
+        return self.get(pkey, default=default)
+
+    def put_data(self, model_cls, pkey, data, create_new):
+        return self.set(pkey, data)
+
+    def add(self, key, value, timeout=0, min_compress=50):
+        """
+        timeout:过期时间
+        min_compress:压缩
+        """
+        return self.master.set(key, value, px=timeout)
